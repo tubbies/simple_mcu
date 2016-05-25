89a63f625d0f5c967ed0ffa98d644bc813237c1b
tubbies <tubbieskjm@gmail.com>
2016-05-25 19:49:00 +0900

    Update alu_core_if.v

--- tb/alu_core_if.v
+++ tb/alu_core_if.v
@@ -3,17 +3,12 @@ interface alu_core_if
     parameter   D_BW    =   8       ,//
     parameter   I_BW    =   4        //
 )
-(   
-    input wire              clk     ,//
-    input wire              rst_n    //
-);
+//(   
+//    input wire              clk     ,//
+//    input wire              rst_n    //
+//);
     logic                   i_en    ;
     logic [ I_BW    -1:0]   i_cmd   ;
     logic [ D_BW    -1:0]   i_da    ;
     logic [ D_BW    -1:0]   i_db    ;
-    
-    logic                   o_en    ;
-    logic                   o_of    ;
-    logic                   o_ofb   ;
-    logic [ D_BW    -1:0]   o_dat   ;
 endinterface: alu_core_if
