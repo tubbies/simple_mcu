cbd7a03e6a89a346320af229694164e55f79a640
tubbies <tubbieskjm@gmail.com>
2016-05-25 19:56:14 +0900

    Update alu_core_if.sv

--- rtl/alu_core_if.sv
+++ rtl/alu_core_if.sv
@@ -3,7 +3,10 @@ interface alu_core_if
     parameter   I_BW        =               4                       ,// Instruction Bandwidth
     parameter   D_BW        =               4                        // Data Bandwidth
 );
-
+//(   
+//    input wire              clk     ,//
+//    input wire              rst_n    //
+//);
     logic                                   i_en                    ;
     logic       [   I_BW    -1:0]           i_cmd                   ;
     logic       [   D_BW    -1:0]           i_da                    ;
