ec6318efa9f98851ad8b638469602d1a73b22daa
tubbies <tubbieskjm@gmail.com>
2019-08-30 13:39:36 +0900

    Create test_dpi.sv

---
+++ test_dpi.sv
@@ -0,0 +1,5 @@
+//inside TB module
+import "DPI-C" context function void function_name();
+// use imported function in SystemVerilog
+// export "DPI-C" context task sv_function_name();
+// use exported function in C
