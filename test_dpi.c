4e49777728a9b6790bd4e7db0141ab3c34a344e9
tubbies <tubbieskjm@gmail.com>
2019-08-30 11:49:11 +0900

    Create test_dpi.c

---
+++ test_dpi.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+// DPI-C File variables
+int var;
+
+#ifdef __cplusplus
+extern "C" {
+// function header
+#endif
+#ifdef __cplusplus
+}
+#endif
+//function description
